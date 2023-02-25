d9d6d154591167431ff524b07b4ac23a4b960338
Luna <swagnemite@gmx.de>
2023-02-25 02:56:31 +0100

    BF 109 => BF-109

--- lua/entities/lvs_plane_bf109/shared.lua
+++ lua/entities/lvs_plane_bf109/shared.lua
@@ -1,7 +1,7 @@
 
 ENT.Base = "lvs_base_fighterplane"
 
-ENT.PrintName = "BF 109"
+ENT.PrintName = "BF-109"
 ENT.Author = "Luna"
 ENT.Information = "German World War 2 Fighterplane"
 ENT.Category = "[LVS] - Planes"
