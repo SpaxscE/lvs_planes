755cddca8d177900856f022843b91f512e6805b1
Luna <24762027+SpaxscE@users.noreply.github.com>
2024-12-31 18:23:17 +0100

    for how long has this been inverted?

--- lua/entities/lvs_plane_zero/cl_init.lua
+++ lua/entities/lvs_plane_zero/cl_init.lua
@@ -42,8 +42,8 @@ function ENT:AnimControlSurfaces( frametime )
 	self.smYaw = self.smYaw and self.smYaw + (Yaw - self.smYaw) * FT or 0
 	self.smRoll = self.smRoll and self.smRoll + (Roll - self.smRoll) * FT or 0
 
-	self:ManipulateBoneAngles( 13, Angle( 0,0,-self.smRoll) )
-	self:ManipulateBoneAngles( 4, Angle( 0,0,self.smRoll) )
+	self:ManipulateBoneAngles( 13, Angle( 0,0,self.smRoll) )
+	self:ManipulateBoneAngles( 4, Angle( 0,0,-self.smRoll) )
 	
 	self:ManipulateBoneAngles( 5, Angle( 0,0,-self.smPitch) )
 	
