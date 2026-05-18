1316a03d0dfc084ff462d10aa3017b57c387b48e
SpaxscE <stephanklings@freenet.de>
2026-05-18 09:57:51 +0200

    Update init.lua

--- lua/entities/lvs_plane_template/init.lua
+++ lua/entities/lvs_plane_template/init.lua
@@ -113,15 +113,15 @@ function ENT:DoMissileDistraction()
 
 
 	-- or do all manually:
-	--
-	--	self:SetMissileNoTarget( 2 ) -- this sets how many seconds we can not be found by missiles. Value doesn't have to be high as the flares themself act as distraction aswell
-	--
-	--	-- self:CreateFlare( PosWorld, DirWorld, Velocity )
-	--	local FlareEnt = self:CreateFlare( self:LocalToWorld( Vector(0,0,50) ), self:GetForward(), 1200 )
-	--
-	--	-- play funny sound
-	--	self:EmitSound("lvs/diprip_countermeasure.wav",85,100,0.25)
+	--[[
+		self:SetMissileNoTarget( 2 ) -- this sets how many seconds we can not be found by missiles. Value doesn't have to be high as the flares themself act as distraction aswell
 
+		-- self:CreateFlare( PosWorld, DirWorld, Velocity )
+		local FlareEnt = self:CreateFlare( self:LocalToWorld( Vector(0,0,50) ), self:GetForward(), 1200 )
+
+		-- play funny sound
+		self:EmitSound("weapons/flaregun/fire.wav")
+	--]]
 
 
 	-- cooldown before next fire, 6 seconds in this case
